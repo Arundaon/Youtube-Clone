5638047425252e3f6bf1bfb48de0ff3d1065e495
Arundaon <arydnn18@gmail.com>
2024-06-09 21:24:39 +0700

    add upload profile feature, add bio in user userResponse DTO

--- src/main/java/arundaon/ytclone/models/UpdateUserRequest.java
+++ src/main/java/arundaon/ytclone/models/UpdateUserRequest.java
@@ -1,11 +1,13 @@
 package arundaon.ytclone.models;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,4 +19,5 @@ public class UpdateUserRequest {
     @Size(min = 3, max = 16)
     private String name;
     private String bio;
+    private MultipartFile profile;
 }
